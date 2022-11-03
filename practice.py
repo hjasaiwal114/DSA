f73ea31e2c6b3b805e8f3660e1907803fb60732d
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2022-11-04 04:15:13 +0530

    Add files via upload

--- practice.py
+++ practice.py
@@ -1,115 +1,81 @@
-# def fact(n):
-#     if n == 0:
-#         return 1
+# class Node:
+#     def __init__(self,k):
+#         self.key = k
+#         self.next = None
 
-#     fnm1 = int(fact(n-1))
-#     fn = int(n*fact(n-1))
-#     return fn
-# print(fact(5))
+# temp1 = Node(10)
+# temp2 = Node(20)
+# temp3 = Node(30)
+# temp4 = Node(40)
 
+# temp1.next = temp2
+# temp2.next = temp3
+# temp3.next = temp4
 
-# def sum(n):
-#     if n ==1:
-#         return 1
+# head = temp1
+# print(head.key)
+# print(head.next.key)
+# print(head.next.next.key)
+# print(head.next.next.next.key)
 
-#     snm1 = sum(n-1)
-#     sn = n + snm1
-#     return sn
-# print(sum(5))
+# class Node:
+#     def __init__(self,key):
+#         self.key = key
+#         self.next = None
+# def printList(head):
+#     curr = head
+#     while curr != None:
+#         print(curr.key,end =" ")
+#         curr = curr.next
 
-
-# def fib(n):
-#     if n == 0 or n ==1:
-#         return n
-
-#     fnm1 = fib(n-1)
-#     fnm2 = fib(n-2)
-#     fn =  fnm1 + fnm2
-#     return fn 
-# print(fib(25))
-
-# def  isSorted(arr, i):
-#     if i == len(arr)-1:
-#         return True
-#     if arr[i] > arr[i+1]:
-#         return False
-#     return isSorted(arr ,i+1)
-
-# arr = [1,2,0,4]
-# print(isSorted(arr ,0))
-
-# def firstOccurence(arr,key,i):
-#     if i == len(arr):
-#         return -1
-#     if arr[i] == key:
-#         return i  
-#     return firstOccurence(arr , key, i+1)
-# arr = [8,3,6,9,5,10,2,5,3]
-# print(firstOccurence(arr,5,0))
+# head = Node(10)
+# head.next = Node(20)
+# head.next.next = Node(15)
+# head.next.next.next = Node(30)
+# printList(head)  
 
 
-# def power(x,n):
-#     if n == 0:
-#         return 1
-#     xnm1 = power(x,n-1)
-#     xn = x* xnm1
-#     return xn
-# print(power(2,10))
+class Node:
+    def __init__(self, key):
+        self.key = key
+        self.next = None
+def search(head,x):
+    pos = 1
+    curr = head
+    while curr!= None:
+        if curr.Key == x:
+            return pos
+        pos = pos + 1
+        curr = curr.next
+    return -1
 
-# def optimizedPower(a,n):
-#     if n == 0:
-#         return 1
-#     halfpowerSq = optimizedPower(a,n//2) 
-#     halfpowerSq = halfpowerSq*halfpowerSq
-#     # n is odd
-#     if n%2 != 0:
-#         halfpowerSq = a*halfpowerSq
-#     return halfpowerSq
-# print(optimizedPower(2,10))
+head = Node(10)
+head.next = Node(15)
+head.next.next = Node(20)
+head.next.next.next = Node(25)
+x = 20
+print(search(head, x))
 
-# def tillingProblem(n): # 2 * n (floor size)
-#     # base case
-#     if n ==0 or n==1 :
-#         return 1
-
-#     # kaam
-#     # vertical choice
-
-#     # vertical choice
-#     fnm1 = tillingProblem(n-1)
-#     # hoizontal choice
-#     fnm2 = tillingProblem(n-2)
-
-#     totways = fnm1 + fnm2
-#     return totways
-# print(tillingProblem(4))
-
-# def removeDuplicates(str, idx , newStr , map):
-#     if idx == len(str):
-#         print(newStr)
-#     # kaam
-#     currChar = str[idx]
-#     if currChar in map :
-#         # duplicate
-#         removeDuplicates(str,idx+1,newStr,map)
-#     else:
-#         removeDuplicates(str ,idx+1, newStr.append(currChar), map)
-
-# print(removeDuplicates("appppnnacollege",0," ",[26] ))  # na bhai na ye question ek bar python m delh lio
+# class Node:
+#     def __init__(self, key):
+#         self.key = key
+#         self.next = None
 
 
-def  friendsparing(n):
-    if n ==1 or n ==2 :
-        return n
-    #choice
-    #single
-    fnm1 = friendsparing(n-1)
+# def search(head, x):
+#     pos = 1
+#     curr = head
+#     while curr != None:
+#         if curr.key == x:
+#             return pos
+#         pos = pos + 1
+#         curr = curr.next
+#     return -1
 
-    #pair
-    fnm2 = friendsparing(n-2)
-    pairwaWays = (n-1)*fnm2
 
-    #toWays
-    totways = fnm1 + pairwaWays
-    return totways
-print(friendsparing(3))
+# head = Node(10)
+# head.next = Node(15)
+# head.next.next = Node(20)
+# head.next.next.next = Node(25)
+# x = 20
+# print(search(head, x))
