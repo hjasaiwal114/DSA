f179e30301c7da9c0a99194a7cb9466636912d1f
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2023-03-19 21:31:38 +0000

    Hashing in python with linear probing

---
+++ Dsa_revision/Hashing.py
@@ -0,0 +1,59 @@
+class HashTable:
+    def __init__(self, size):
+        self.size = size
+        self.keys = [None] * size
+        self.values = [None] * size
+
+    def _hash_function(self, key):
+        return hash(key) % self.size
+
+    def _linear_probe(self, index):
+        return (index + 1) % self.size
+
+    def put(self, key, value):
+        hash_value = self._hash_function(key)
+        if self.keys[hash_value] is None:
+            self.keys[hash_value] = key
+            self.values[hash_value] = value
+        else:
+            index = self._linear_probe(hash_value)
+            while self.keys[index] is not None and self.keys[index] != key:
+                index = self._linear_probe(index)
+            if self.keys[index] == key:
+                self.values[index] = value
+            else:
+                self.keys[index] = key
+                self.values[index] = value
+
+    def get(self, key):
+        hash_value = self._hash_function(key)
+        if self.keys[hash_value] == key:
+            return self.values[hash_value]
+        else:
+            index = self._linear_probe(hash_value)
+            while self.keys[index] is not None and self.keys[index] != key:
+                index = self._linear_probe(index)
+            if self.keys[index] == key:
+                return self.values[index]
+            else:
+                return None
+
+    def delete(self, key):
+        hash_value = self._hash_function(key)
+        if self.keys[hash_value] == key:
+            self.keys[hash_value] = None
+            self.values[hash_value] = None
+        else:
+            index = self._linear_probe(hash_value)
+            while self.keys[index] is not None and self.keys[index] != key:
+                index = self._linear_probe(index)
+            if self.keys[index] == key:
+                self.keys[index] = None
+                self.values[index] = None
+
+    def __str__(self):
+        res = []
+        for i in range(self.size):
+            if self.keys[i] is not None:
+                res.append(f"{self.keys[i]}: {self.values[i]}")
+        return "{" + ", ".join(res) + "}"
