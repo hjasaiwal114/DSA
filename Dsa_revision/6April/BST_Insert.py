3aa0638535ea10be783bc755d626009c3b2a8169
himanshu jasaiwal <68457116+hjasaiwal114@users.noreply.github.com>
2023-04-06 17:34:01 +0000

    bst insert

---
+++ Dsa_revision/6April/BST_Insert.py
@@ -0,0 +1,8 @@
+def insert(root, val):
+    if not root:
+        return TreeNode(val)
+    if val > root.val:
+        root.right = insert(root.right, val)
+    elif val < root.val:
+        root.left = insert(root.left, val)
+    return root
