0170b3a37cd80040c86e4ebde8328bbe06bb4f52
himanshu jasaiwal <68457116+hjasaiwal114@users.noreply.github.com>
2023-04-06 18:10:19 +0000

    remove function

--- Dsa_revision/6April/BST_Insert.py
+++ Dsa_revision/6April/BST_Insert.py
@@ -7,3 +7,57 @@ def insert(root, val):
         root.left = insert(root.left, val)
     return root
 
+def minValueNode(root):
+    curr = root
+    while curr and curr.left:
+        curr = curr.left
+    return curr
+
+def remove(root, val):
+    if not root:
+        return None
+    if val > root.val:
+        root.right = remove(root.right, val)
+    elif val < root.val:
+        root.left = remove(root.left, val)
+    else:
+        if not root.left:
+            return root.right
+        elif not root.right:
+            return root.left
+        else:
+            minNode = minValueNode(root.right)
+            root.val =minNode.val
+            root.right = remove(root.right, minNode.val)
+    return root
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
