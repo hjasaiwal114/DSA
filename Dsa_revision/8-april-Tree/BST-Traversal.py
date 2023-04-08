9e5c93c524172ffbf3088b92f1819fe497b8acb6
himanshu jasaiwal <68457116+hjasaiwal114@users.noreply.github.com>
2023-04-08 11:46:54 +0000

    preorder, postorder

--- Dsa_revision/8-april-Tree/BST-Traversal.py
+++ Dsa_revision/8-april-Tree/BST-Traversal.py
@@ -4,3 +4,17 @@ def inorder(root):
     inorder(root.left)
     print(root.val)
     inorder(root.right)
+
+def preorder(root):
+    if not root:
+        return
+    print(root.val)
+    preorder(root.left)
+    preorder(root.right)
+
+def postorder(root):
+    if not root:
+        return
+    postorder(root.left)
+    postorder(root.right)
+    print(root.val)
