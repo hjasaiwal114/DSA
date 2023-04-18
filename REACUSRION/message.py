3cd3197d8f4567355ea2687c70d308023cf6748a
himanshu jasaiwal <68457116+hjasaiwal114@users.noreply.github.com>
2023-04-18 11:28:31 +0000

    making recusrive call while adding base case

--- REACUSRION/message.py
+++ REACUSRION/message.py
@@ -1,3 +1,10 @@
 # write a function that takes in a number and prints it
 # print first 5 numbers: 1 2 3 4 5
 
+def print_n(n):
+    if n == 5:
+        print(5)
+    return
+print_n(5)
+n = 10
+print(n+1)
