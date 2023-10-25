1f30b61ffdda99dce6d02ee812018f25a0412244
Himanshu jasaiwal <hjasaiwal114@gmail.com>
2023-10-26 03:05:44 +0530

    queu done

---
+++ Dsa_typescript/src/queue.ts
@@ -0,0 +1,44 @@
+type QNode<T> = {
+    value: T,
+    next?: Node<T>,
+}
+export default class Queue<T> {
+    public length: number;
+    private head?: Node<T>;
+    private tail?: Node<T>;
+
+    constructor() {
+        this.head = this.tail = undefined;
+        this.length = 0;
+    }
+
+    enqueue(item: T): void {
+        const node = {value: item} as Node<T>;
+        this.length++
+        if (!this.tail) {
+            this.tail = this.head = node;
+            return;
+        }
+        this.tail.next = node;
+        this.tail = node;
+
+    }
+    deque(): T | undefined {
+        if ( !this.head) {
+            return undefined;
+        }
+
+        this.length--;
+
+        const head = this.head;
+        this.head = this.head.next;
+
+        head.next = undefined;
+
+        return head.value;
+
+    }
+    peek() : T | undeined {
+        return this.head?.value;
+    }
+}
