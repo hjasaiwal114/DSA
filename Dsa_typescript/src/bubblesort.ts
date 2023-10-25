0d5e0dcf25d8955fbcfd04dcc7a785855e152b9c
Himanshu jasaiwal <hjasaiwal114@gmail.com>
2023-10-25 23:58:38 +0530

    error handeling

--- Dsa_typescript/src/bubblesort.ts
+++ Dsa_typescript/src/bubblesort.ts
@@ -3,7 +3,8 @@ export default function Buuble_sort(arr: number[]) : void {
         for (let j = 0; j< arr.length - 1 - i; ++j) {
             
             if (arr[j] > arr[j+ 1]) {
-                const tmp = arr[j]
+                const tmp = arr[j];
+                arr[j] = arr[j+ 1];
                 arr[j + 1] = tmp;
             }
         }
